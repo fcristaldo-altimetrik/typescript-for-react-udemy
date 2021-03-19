801afef4fe56e76fc633ba31298792ccce084ccc
Facundo <facucristaldorodriguez@gmail.com>
2021-03-19 09:27:57 -0300

    adjustments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { runModules } from "./Modules/moduleUse";
 import { runTypes } from "./Types/Types";
 import { runInterfaces } from "./Interfaces/Interfaces";
 
+console.log("\n\n---------- START ----------");
 runModules();
 runTypes();
 runInterfaces();
+console.log("\n\n---------- END ----------");
