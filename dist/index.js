5d78b6f62180261ff1aaee334922a3762d81a737
fcristaldo-altimetrik <80272535+fcristaldo-altimetrik@users.noreply.github.com>
2021-03-19 09:24:24 -0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const moduleUse_1 = require("./Modules/moduleUse");
-const Types_1 = require("./Types/Types");
-const Interfaces_1 = require("./Interfaces/Interfaces");
-moduleUse_1.runModules();
-Types_1.runTypes();
-Interfaces_1.runInterfaces();
