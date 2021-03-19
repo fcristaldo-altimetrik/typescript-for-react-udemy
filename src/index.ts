0e2ff3b78c55501f35db613b2a4d8fc28a4aef5c
Facundo <facucristaldorodriguez@gmail.com>
2021-03-19 09:39:36 -0300

    functions params, return type, overloads, ...args

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { runModules } from "./Modules/moduleUse";
 import { runTypes } from "./Types/Types";
 import { runInterfaces } from "./Interfaces/Interfaces";
+import { runFunctions } from "./Functions/Functions";
 
 runModules();
 runTypes();
 runInterfaces();
+runFunctions();
