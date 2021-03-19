70b160512cc884342b47d04c1fe5ecac3f0ed90c
Facundo <facucristaldorodriguez@gmail.com>
2021-03-19 09:40:47 -0300

    resolved conflics

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { runModules } from "./Modules/moduleUse";
 import { runTypes } from "./Types/Types";
 import { runInterfaces } from "./Interfaces/Interfaces";
+import { runFunctions } from "./Functions/Functions";
 
 console.log("\n\n---------- START ----------");
 runModules();
 runTypes();
 runInterfaces();
+runFunctions();
 console.log("\n\n---------- END ----------");
